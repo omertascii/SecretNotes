56e4df5b66c4e11f2a33c26103281d940370e898
Omer Faruk Taşçı <omertasci844@gmail.com>
2023-08-11 18:32:19 +0300

    implementation completed

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import tkinter
 from tkinter import messagebox, INSERT, END
 from cryptography.fernet import Fernet
-
+import base64
 
 screen = tkinter.Tk()
 screen.title("Secret Notes")
@@ -31,6 +31,22 @@ password = tkinter.Entry()
 password.pack()
 
 
+def encode(key, clear):
+    enc = []
+    for i in range(len(clear)):
+        key_c = key[i % len(key)]
+        enc_c = chr((ord(clear[i]) + ord(key_c)) % 256)
+        enc.append(enc_c)
+    return base64.urlsafe_b64encode("".join(enc).encode()).decode()
+
+def decode(key, enc):
+    dec = []
+    enc = base64.urlsafe_b64decode(enc).decode()
+    for i in range(len(enc)):
+        key_c = key[i % len(key)]
+        dec_c = chr((256 + ord(enc[i]) - ord(key_c)) % 256)
+        dec.append(dec_c)
+    return "".join(dec)
 
 
 def savebutton_click():
@@ -40,20 +56,33 @@ def savebutton_click():
     if num1 == "" or num2 == "" or num3 == "":
         messagebox.showinfo(title="Message",message="Boş alanları doldurunuz")
     else:
+        mynote_encrypted = encode(num3,num2)
         secret = open("mysecret.txt",mode="a")
         secret.write(num1)
         secret.write("\n")
-        secret.write(num2)
+        secret.write(mynote_encrypted)
         secret.write("\n")
         file_name.delete(0, END)
         my_note.delete("1.0", END)
         password.delete(0, END)
 
+def decypt_button():
+    num1 = my_note.get("1.0", END)
+    num2 = password.get()
+    if num1 == "" or num2 =="":
+        messagebox.showinfo(title="Hata!",message="Boş alanları doldurunuz")
+    else:
+        try:
+            decyrpted_message = decode(num2,num1)
+            my_note.delete("1.0", END)
+            my_note.insert("1.0",decyrpted_message)
+        except:
+            messagebox.showinfo(title="Hata!",message="Şifrelenmemiş Metni Çözmeye Çalışma!")
 
 file_save = tkinter.Button(text="Save",width=20,command=savebutton_click)
 file_save.pack()
 
-decrypt = tkinter.Button(text="Decrypt",width=10)
+decrypt = tkinter.Button(text="Decrypt",width=10,command=decypt_button)
 decrypt.pack()
 
 
